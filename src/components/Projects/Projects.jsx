2e9cf7d1c32ff7dd193ffa9aeb606db1782efe96
hocine1212 <hocine20992@gmail.com>
2023-12-05 20:02:03 +0100

    style the navbar

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="section" id="projects">
+      Projects
+    </div>
+  );
 };
 
 export default Projects;
