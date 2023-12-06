17309c75ee476372da8a01fe497a71d52a75135b
hocine1212 <hocine20992@gmail.com>
2023-12-06 14:39:30 +0100

    Implement the Header component and style it

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Link from "next/link";
 
 const Header = () => {
-  return <div className="bg-pink-300">Header</div>;
+  return (
+    <header className="py-8 md:px-16">
+      <div className="p-[15px] mx-auto">
+        <div className="flex justify-between items-center">
+          <Link
+            href="#"
+            className="text-3xl text-white font-extrabold hover:underline focus:outline-none focus:ring focus:border-blue-300 
+            "
+          >
+            <span>&lt;&gt;</span>Hocine.
+          </Link>
+          <button className="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-pink-500 hover:to-purple-500 px-3 font-bold py-2 rounded-full transition-all duration-300 focus:outline-none focus:ring focus:border-blue-300">
+            Work with me
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
