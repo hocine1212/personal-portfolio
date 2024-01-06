3c44c5a7a630295398d760180370d174929d3a0d
hocine1212 <hocine20992@gmail.com>
2024-01-06 15:19:07 +0100

    correct the name of the environement variable

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
         process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
         process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
         form.current,
-        process.env.NEXT_PUBLIC_EMAILJS_USER_ID
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
